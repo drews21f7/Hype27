8400275387ade43200602e6ca40e1783175af4ca
Drew Seeholzer <drews21f7@gmail.com>
2019-07-09 11:01:01 -0600

    Added model

---
+++ Hype27/Models/Hype.swift
@@ -0,0 +1,44 @@
+//
+//  Hype.swift
+//  Hype27
+//
+//  Created by Drew Seeholzer on 7/9/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct Constants {
+    static let recordTypeKey = "Hype"
+    fileprivate static let recordTextKey = "Text"
+    static let recordTimestampKey = "Timestamp"
+    
+}
+
+class Hype {
+    let hypeText: String
+    let timestamp: Date
+    
+    init(hypeText: String, timestamp: Date) {
+        self.hypeText = hypeText
+        self.timestamp = timestamp
+        
+    }
+}
+    // Creating a Hype from a record
+extension Hype {
+    convenience init?(ckRecord: CKRecord) {
+        guard let hypeText = ckRecord[Constants.recordTextKey] as? String, let hypeTimestamp = ckRecord[Constants.recordTimestampKey] as? Date else { return nil }
+        self.init(hypeText: hypeText, timestamp: hypeTimestamp)        
+    }
+}
+
+// Creating a CKRecord frmo a Hype
+extension CKRecord {
+    convenience init(hype: Hype) {
+        self.init(recordType: Constants.recordTypeKey)
+        self.setValue(hype.hypeText, forKey: Constants.recordTextKey)
+        self.setValue(hype.timestamp, forKey: Constants.recordTimestampKey)
+    }
+}
